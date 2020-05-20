86d075512cec436f4b25316cd495cb1e5a1131ed
David Torres <dftp93@gmail.com>
2020-05-20 18:01:59 -0500

    Added last sections and footer

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,8 @@
-// alert('Estable');
+function lazyLoad() {
+  var allImages = document.querySelectorAll("img");
+  for (var i = 0; i < allImages.length;  i++) {
+    allImages[i].setAttribute("loading", 'lazy');
+  }
+};
+
+lazyLoad();
