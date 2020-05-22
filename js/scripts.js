a1304498e68c2efc00971d811d367dd91988a585
David Torres <dftp93@gmail.com>
2020-05-22 10:49:28 -0500

    Added search overlay on click input main

--- js/scripts.js
+++ js/scripts.js
@@ -6,3 +6,16 @@ function lazyLoad() {
 };
 
 lazyLoad();
+
+var openOverlay = document.getElementById("input-modal"),
+    overlay = document.getElementById("modal-search"),
+    closeOverlay = document.getElementById("close-search");
+
+openOverlay.addEventListener('click', function (e) {
+  e.preventDefault();
+  overlay.classList.add("active");
+})
+closeOverlay.addEventListener('click', function (e) {
+  e.preventDefault();
+  overlay.classList.remove("active");
+})
